7d2e35ab4a436298a94db7f5be7392595b99117e
Sean DeNigris <sean@clipperadams.com>
2011-12-20 15:25:31 -0500

    Initial Commit

---
+++ TestPharoUI/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  TestPharoUI
+#
+#  Created by Sean DeNigris on 12/20/11.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+

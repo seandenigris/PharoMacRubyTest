f9bbfb1eaed56a83ccbf415cd8aa955e8896240a
Sean DeNigris <sean@clipperadams.com>
2011-12-20 21:18:44 -0500

    Proof of concept for Pharo app with MacRuby Cocoa GUI

--- TestPharoUI/AppDelegate.rb
+++ TestPharoUI/AppDelegate.rb
@@ -6,10 +6,18 @@
 #  Copyright 2011 __MyCompanyName__. All rights reserved.
 #
 
+require 'net/http'
+
 class AppDelegate
     attr_accessor :window
+    attr_writer :time_field
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
+    end
+    def windowWillClose(sender); exit; end
+    def showTime(sender)
+        response = Net::HTTP.start("127.0.0.1", 1701) { |http| http.get('/') }
+        @time_field.stringValue = response.body
     end
 end
 
